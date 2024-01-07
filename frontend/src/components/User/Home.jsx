64ea0d1317a3ca97fa0e0fd05d77bd97b1fbbf20
Abhishek Negi <112553331+Abhih21@users.noreply.github.com>
2024-01-07 23:40:37 +0530

    update

--- frontend/src/components/User/Home.jsx
+++ frontend/src/components/User/Home.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import truck_bg from "../../assets/image/truck_bg.png";
 
 function Home() {
   return (
     <div>
-      home Page
+        <section className="bg-[#eeefefab] h-screen">
+        <div className="flex ">
+          <div className="flex flex-col  p-32 gap-14 mr-6">
+            <div className="max-w-690 flex flex-col pt-28  gap-3">
+              <h1 className="font-extrabold text-5xl absolute text-black">
+                On demand truck <br /> transport and deliveries
+              </h1>
+              <p className="font-medium text-2xl pt-28 absolute text-black-100">
+                Have moving or delivery needs?  We help you <br /> schedule at an affordable price
+              </p>
+            </div>
+            <div className=''>
+            <div className="flex gap-2 absolute pt-36 w-4/5 ">
+              <Link to={"/"}>
+               <input type="text" className="border border-gray-500 h-10"
+                 placeholder='Enter Track & Trace ID'
+                />
+              </Link>
+              <Link to={"./profile/places"}>
+                <button className="border rounded-lg text-white p-2 mt-0.5 h-10 capitalize font-medium  bg-violet-600 hover:bg-violet-500 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 hover:text">
+                  Check Status
+                </button>
+              </Link>
+            </div>
+            </div>
+          </div>
+          <div className=" pl-72">
+            <img src={truck_bg} alt="" className="h-full w-full" />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
