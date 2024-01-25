6954250ed45382d7a5b10460ad74e515c4ff1d88
Abhishek Negi <112553331+Abhih21@users.noreply.github.com>
2024-01-25 21:25:51 +0530

    password

--- frontend/src/components/User/Register.jsx
+++ frontend/src/components/User/Register.jsx
@@ -52,13 +52,11 @@ function Register() {
   }
   return (
     <>
-      <form className="container border m-24">
+      <form className="container bg-slate-200 border m-24">
         <div className="space-y-12 ">
           <div className="border-b border-gray-900/10 pb-12">
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               
-
-             
 
               <div className="col-span-full ml-96">
                 <label
@@ -288,6 +286,25 @@ function Register() {
                     />
                   </div>
                 </div>
+                <label
+                  htmlFor="username"
+                  className="block mt-10 text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      autoComplete="password"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1  text-gray-900 placeholder:text-gray-400 
+                       focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="password"
+                      value={credential.userName}
+                      onChange={handleChange}
+                    />
+                  </div>
               </div>
 
                <div className="col-span-full pl-24">
